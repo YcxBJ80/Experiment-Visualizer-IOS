53b711fd2db7b8451bd9e8f5ad31f463662ccb41
YcxBJ80 <yangchengxuan08@icloud.com>
2025-12-06 19:46:00 +0800

    Initial Commit

---
+++ Experiment Visualizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Experiment Visualizer
+//
+//  Created by 杨承轩 on 2025/12/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Experiment Visualizer/Experiment_VisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Experiment_VisualizerApp.swift
+//  Experiment Visualizer
+//
+//  Created by 杨承轩 on 2025/12/6.
+//
+
+import SwiftUI
+
+@main
+struct Experiment_VisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
